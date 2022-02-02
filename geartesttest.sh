4c228775f0ef49cd3dae5ef86bac7a39d4e24a85
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-02 17:29:06 +0400

    Update geartesttest.sh

--- geartesttest.sh
+++ geartesttest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo apt install curl
+curl https://sh.rustup.rs -sSf | sh
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install dos2unix
-sudo apt install curl
 sudo apt install -y git clang curl libssl-dev llvm libudev-dev
 sudo apt-get install nano
 sudo apt install cargo
-curl --proto --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 
 curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
 echo "You have run the Crypton Academy Gear node setup script "
