b35766cbed17e68fe8c8a70accd55bfcc9da1806
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-05 17:05:59 +0400

    Update gearfinalmb.sh

--- gearfinalmb.sh
+++ gearfinalmb.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+su
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git clang curl libssl-dev llvm libudev-dev
 
 
- echo "Press 1 "
-# Install
 curl https://sh.rustup.rs -sSf | sh
-# Configure
 source ~/.cargo/env
+
 
 rustup default stable
 rustup update
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
 
-cd
-git clone https://github.com/gear-tech/gear.git
-cd gear/node
-cargo build -–release
+#добавление названия ноды
+echo "write your node name ↓↓↓"
+read NAME
 
 
-cd /etc/systemd/system
+echo "installing gear node..."
+wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz && \
+tar xvf gear-nightly-linux-x86_64.tar.xz && \
+rm gear-nightly-linux-x86_64.tar.xz && \
+chmod +x $HOME/gear-node
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/gear-node.service
+cargo build --release
+   Compiling gear-node v0.1.0 (/root/gear-node)
+    Building [=======================> ] 928/930: gear-node
+    
+    
+
+    tee <<EOF >/dev/null /etc/systemd/system/$NAME.service
+[Unit]
 Description=Gear Node
 After=network.target
 
@@ -30,23 +39,18 @@ After=network.target
 Type=simple
 User=root
 WorkingDirectory=/root/
-ExecStart=/root/gear-node \
-        name gear-node \
-        execution wasm \
-        log runtime
+ExecStart=/root/gear-node -name papadritta -execution wasm -log runtime
 Restart=always
 RestartSec=3
 LimitNOFILE=10000
 
 [Install]
 WantedBy=multi-user.target
+
 EOF
 
 
-sudo cp gear-node /root
+
 systemctl daemon-reload
-systemctl start gear-node
-systemctl status gear-node
-
-
-journalctl -n 100 -f -u gear-node
+systemctl enable minima_$PORT
+systemctl start minima_$PORT
