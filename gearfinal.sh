03a448185743654ec652b47a714cd87e09d2b61c
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-18 02:05:21 +0400

    Update gearfinal.sh

--- gearfinal.sh
+++ gearfinal.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
+
+echo -e '\e[40m\e[91m'
+echo -e '  ____                  _                    '
+echo -e ' / ___|_ __ _   _ _ __ | |_ ___  _ __        '
+echo -e '| |   |  __| | | |  _ \| __/ _ \|  _ \       '
+echo -e '| |___| |  | |_| | |_) | || (_) | | | |      '
+echo -e ' \____|_|   \__  |  __/ \__\___/|_| |_|      '
+echo -e '            |___/|_|                         '
+echo -e '    _                 _                      '
+echo -e '   / \   ___ __ _  __| | ___ _ __ ___  _   _ '
+echo -e '  / _ \ / __/ _  |/ _  |/ _ \  _   _ \| | | |'
+echo -e ' / ___ \ (_| (_| | (_| |  __/ | | | | | |_| |'
+echo -e '/_/   \_\___\__ _|\__ _|\___|_| |_| |_|\__  |'
+echo -e '                                       |___/ '
+echo -e '\e[0m'
+
+sleep 1
+
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git clang curl libssl-dev llvm libudev-dev
-
-echo "Press 1 when the selection window appears"
-curl https://sh.rustup.rs -sSf | sh
-source ~/.cargo/env
-rustup default stable
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
-wget -q -O -P /etc/systemd/system "https://raw.githubusercontent.com/Zhoas/gearnodetesttest/main/gear-node.service"
-
 
 wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz && \
 tar xvf gear-nightly-linux-x86_64.tar.xz && \
 rm gear-nightly-linux-x86_64.tar.xz && \
-chmod +x $HOME/gear-node
-
-cargo build --release
+chmod +x gear-node
 
 
+./gear-node
 
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable gear-node
-sudo systemctl restart gear-node
 
 sudo systemctl status gear-node
-
 sudo journalctl -n 100 -f -u gear-node
