d19486424ad5086583decd4f9e3a6d2a082db5a6
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-19 17:11:17 +0400

    Update gearfinal.sh

--- gearfinal.sh
+++ gearfinal.sh
@@ -30,6 +30,36 @@ git clone https://github.com/gear-tech/gear
 cd gear
 make node-release
 
+tee <<EOF >/dev/null /etc/systemd/system/gear-node.service
+[Unit]
+Description=Gear Node
+After=network.target
 
-./gear-node
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root/
+ExecStart=/root/gear-node \
+        --name papadritta \
+        --execution wasm \
+        --log runtime
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF 
+
+
+
+
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable gear-node
+sudo systemctl restart gear-node
+
+sudo systemctl status gear-node
+
+sudo journalctl -n 100 -f -u gear-node
 
