767f96a7e62ae378608c06b11f9f042808e00d63
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-24 21:53:45 +0400

    Update gearfinal1.sh

--- gearfinal1.sh
+++ gearfinal1.sh
@@ -2,20 +2,21 @@
 apt-get update 
 apt-get dist-upgrade -y
 apt-get autoremove -y
+apt install make
 apt-get install git curl build-essential -y
 sudo apt install llvm clang
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 source ~/.cargo/env
-rustup default stable
-rustup update
-rustup update nightly
+rustup toolchain add nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
+
 curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
+
 cd
 git clone https://github.com/gear-tech/gear.git
-cd gear/node
-cargo build -p gear-node --release
-cp /root/gear/target/release/gear-node /root/
+cd gear
+make node-release
+sudo cp gear-node /root
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/gear-node.service
 [Unit]
@@ -38,7 +39,6 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-sudo cp gear-node /root
 systemctl daemon-reload
 systemctl start gear-node
 systemctl status gear-node
