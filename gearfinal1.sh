aaa872b3a72d6ee492d17b9e5241f41ad5560a85
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-24 19:11:48 +0400

    Update gearfinal1.sh

--- gearfinal1.sh
+++ gearfinal1.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
-
-curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
-
-
+apt-get update 
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get install git curl build-essential -y
+sudo apt install llvm clang
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
 rustup toolchain add nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
-
-
-wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz && \
-tar xvf gear-nightly-linux-x86_64.tar.xz && \
-rm gear-nightly-linux-x86_64.tar.xz && \
-chmod +x $HOME/gear-node
-
-sudo tee <<EOF >/dev/null /etc/systemd/system/gear-node.service
-[Unit]
-Description=Gear Node
-After=network.target
-
-[Service]
-Type=simple
-User=root
-WorkingDirectory=/root/
-ExecStart=/root/gear-node \
-        --name gear-node \
-        --execution wasm \
-        --log runtime
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=10000
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
-
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable gear-node
-sudo systemctl restart gear-node
-
-sudo systemctl status gear-node
-
-sudo journalctl -n 100 -f -u gear-node
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
+git clone https://github.com/gear-tech/gear.git
+cd gear
+cargo build -p gear-node --release
+wget -q -O /etc/systemd/system"/gear-node.service" "https://raw.githubusercontent.com/Zhoas/gearnodetesttest/main/gear-node.service"
+sudo cp gear-node /root
+systemctl daemon-reload
+systemctl start gear-node
+systemctl status gear-node
+journalctl -n 100 -f -u gear-node
