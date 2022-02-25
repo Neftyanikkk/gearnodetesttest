fe4d201e0a8b700faca9ab882a07e5a8a441a7de
Zhoas <96354836+Zhoas@users.noreply.github.com>
2022-02-25 16:50:41 +0400

    Update gearfinal1.sh

--- gearfinal1.sh
+++ gearfinal1.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y git clang curl libssl-dev llvm libudev-dev
-apt install make
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup toolchain add nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
 git clone https://github.com/gear-tech/gear.git
 cd gear
 make node-release
 
-./gear-node
-journalctl -n 100 -f -u gear-node
+cd /etc/systemd/system
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/gear-node.service
+[Unit]
+Description=Gear Node
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root/
+ExecStart=/root/gear-node \
+        --name gear-node \
+        --execution wasm \
+        --log runtime
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+cd
+cp /root/gear/target/release/gear-node /root/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable gear-node
+sudo systemctl restart gear-node
+
+sudo systemctl status gear-node
+
+sudo journalctl -n 100 -f -u gear-node
