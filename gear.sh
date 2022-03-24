9b14991b7892e9e79e5cde70f90ba207f31d785f
Roman <96354836+Zhoas@users.noreply.github.com>
2022-03-24 17:45:05 +0400

    Update gear.sh

--- gear.sh
+++ gear.sh
@@ -7,7 +7,7 @@ sudo apt install curl git clang libssl-dev llvm libudev-dev -y
 cd $HOME
 rm -rf gear
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32m2. Installing Rust... \e[0m" && sleep 1
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -16,30 +16,30 @@ rustup update
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32m3. Clone https://github.com/gear-tech/gear.git \e[0m" && sleep 1
 git clone https://github.com/gear-tech/gear.git
 cd gear
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32m4. Compile... \e[0m" && sleep 1
 cargo build -p gear-node --release
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32m5. Copy node to /usr/bin/gear-node ... \e[0m" && sleep 1
 sudo rm -rf /usr/bin/gear-node
 sudo cp $HOME/gear/target/release/gear-node /usr/bin/gear-node 
 
-echo "=================================================="
+echo ""∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 echo -e "\e[1m\e[32m6. Enter Gear Node name \e[0m"
 read -p "Node Name : " NODE_NAME
 
 echo -e "\e[1m\e[92m Node Name: \e[0m" $NODE_NAME
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32m7. Creating service for Gear Node \e[0m"
 
@@ -59,7 +59,7 @@ WantedBy=multi-user.target
 
 sudo mv $HOME/gear-node.service /etc/systemd/system
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 # Enabling services
 sudo systemctl restart systemd-journald
@@ -69,11 +69,11 @@ sudo systemctl enable gear-node.service
 # Starting services
 sudo systemctl restart gear-node.service
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32mNode Started \e[0m"
 
-echo "=================================================="
+echo "∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨"
 
 echo -e "\e[1m\e[32mTo stop the Gear Node: \e[0m" 
 echo -e "\e[1m\e[39m    systemctl stop gear-node.service \n \e[0m" 
