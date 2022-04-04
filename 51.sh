87b71add04f19038f1e4ad450adf23ab24f54c23
Roman <96354836+Zhoas@users.noreply.github.com>
2022-04-05 01:12:54 +0400

    Update 51.sh

--- 51.sh
+++ 51.sh
@@ -1,19 +1,84 @@
 #!/bin/bash
-sudo apt update 
-sudo apt install curl -y && apt install jq -y
-clear
-if git > /dev/null 2>&1; then
-	echo ''
-else
-  sudo apt install git
-  echo 'Гит установлен'
-fi
-curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 3
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 2
 
-if docker > /dev/null 2>&1; then
-	echo ''
-else
-  curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-fi
+echo -e "\e[1m\e[32m1. Installing required dependencies... \e[0m" && sleep 1
+sudo apt update
+sudo apt install curl git clang libssl-dev llvm libudev-dev -y
+cd $HOME
+rm -rf gear
 
-echo "=+=+=+=+=+=++=+=++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32m2. Installing Rust... \e[0m" && sleep 1
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32m3. Clone https://github.com/gear-tech/gear.git \e[0m" && sleep 1
+git clone https://github.com/gear-tech/gear.git
+cd gear
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32m4. Compile... \e[0m" && sleep 1
+cargo build -p gear-node --release
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32m5. Copy node to /usr/bin/gear-node ... \e[0m" && sleep 1
+sudo rm -rf /usr/bin/gear-node
+sudo cp $HOME/gear/target/release/gear-node /usr/bin/gear-node 
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+echo -e "\e[1m\e[32m6. Enter Gear Node name \e[0m"
+read -p "Node Name : " NODE_NAME
+
+echo -e "\e[1m\e[92m Node Name: \e[0m" $NODE_NAME
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32m7. Creating service for Gear Node \e[0m"
+
+echo "[Unit]
+Description=Gear Node
+[Service]
+User=$USER
+Type=simple
+ExecStart=gear-node --name '$NODE_NAME'
+Restart=always
+RestartSec=120
+[Install]
+WantedBy=multi-user.target
+" > $HOME/gear-node.service
+
+sudo mv $HOME/gear-node.service /etc/systemd/system
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+# Enabling services
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable gear-node.service
+
+# Starting services
+sudo systemctl restart gear-node.service
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32mNode Started \e[0m"
+
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+
+echo -e "\e[1m\e[32mTo stop the Gear Node: \e[0m" 
+echo -e "\e[1m\e[39m    systemctl stop gear-node.service \n \e[0m" 
+
+echo -e "\e[1m\e[32mTo start the Gear Node: \e[0m" 
+echo -e "\e[1m\e[39m    systemctl start gear-node.service \n \e[0m" 
+
+echo -e "\e[1m\e[32mTo check the Gear Node Logs: \e[0m" 
+echo -e "\e[1m\e[39m    journalctl -u gear-node.service -f \n \e[0m" 
